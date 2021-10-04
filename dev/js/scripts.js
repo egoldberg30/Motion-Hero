fdbb652b74303d246f9c0856feaa56a6231d2223
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-10-04 09:58:36 -0400

    scripts correction

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,6 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+const mainTL = gsap.timeline();
+
+mainTL.from("#heroimage",{duration:2, alpha:0});
+
+mainTL.from("header",{duration:2, alpha:0, x:"-1000"});
