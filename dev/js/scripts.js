cf85bb2b41bb112fb7facfdc4a8da36336339b4a
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-10-06 03:36:16 -0400

    ..

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,7 @@ import { gsap } from "gsap";
 const mainTL = gsap.timeline();
 
 mainTL.from("#hero",{duration:2, alpha:0})
+
 .from("#hero h2",{duration:0.5, alpha:0});
 
-// mainTL.from("header",{duration:2, alpha:0, x:"-1000"});
+mainTL.from("header",{duration:2, alpha:0, x:"-1000"});
