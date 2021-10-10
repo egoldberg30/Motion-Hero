9653a94c8ec779c1629cd4296960ffa93250dca0
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-10-10 17:07:13 -0400

    tweaks

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,8 @@
-import { gsap } from "gsap";
-const mainTL = gsap.timeline();
+//import { gsap } from "gsap";
+//const mainTL = gsap.timeline();
 
-mainTL.from("#hero",{duration:2, alpha:0})
+// mainTL.from("#hero",{duration:2, alpha:0})
 
-.from("#hero h2",{duration:0.5, alpha:0});
+// .from("#hero h2",{duration:0.5, alpha:0});
 
-mainTL.from("header",{duration:2, alpha:0, x:"-1000"});
+// mainTL.from("header",{duration:2, alpha:0, x:"-1000"});
