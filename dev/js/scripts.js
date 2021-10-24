4212f51974667929fe30f1fac9f0f87f8c8c322f
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-10-24 17:26:32 -0400

    js timing update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,6 +3,6 @@ const mainTL = gsap.timeline();
 
 // mainTL.from("#hero",{duration:2, alpha:0});
 
-mainTL.from("#hero h2",{duration:4, alpha:0});
+mainTL.from("#hero h2",{duration:1, alpha:0});
 
-mainTL.from("header",{duration:2, alpha:0, x:"-1000"});
+mainTL.from("header",{duration:1, alpha:0, x:"-1000"});
