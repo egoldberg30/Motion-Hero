395fbdd48daeca6e24c866e8c605ddcd7446f230
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-01 09:17:22 -0400

    morph etc

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,5 @@
 import { gsap } from "gsap";
+import { MorphSVGPlugin } from "MorphSVGPlugin";
 const mainTL = gsap.timeline();
 
 // THIS WAS FROM THE PREVIOUS JS ASSIGNMENT FOR THE HERO //
@@ -10,9 +11,24 @@ const mainTL = gsap.timeline();
 function AllElements(){
     const tl = gsap.timeline();
 
-    tl.from("#All Elements", {duration: 2, x: -1000, ease: "Power3"});
+    tl.from ("#X-ray Board",{duration: 2, x:"-=1000", ease: "Power3"});
 
-    return tl;
+    .to("#Hand 1", { morphSVG:"Hand 2", duration: .1});
+    .to("#Hand 2", { morphSVG:"Hand 2", duration: .1});
+    .to("#Hand 3", { morphSVG:"Hand 3", duration: .1});
+    .to("#Hand 4", { morphSVG:"Hand 4", duration: .1});
+    .to("#Hand 5", { morphSVG:"Hand 5", duration: .1});
+    .to("#Hand 6", { morphSVG:"Hand 6", duration: .1});
+    .to("#Hand 7", { morphSVG:"Hand 7", duration: .1});
+    .to("#Hand 8", { morphSVG:"Hand 8", duration: .1});
+    .to("#Hand 8", { morphSVG:"Hand 9", duration: .1});
+
+    tl.from ("#Button",{duration: .15, y:"-=2", ease: "circ});
+
+    tl.to("#Hand 9", duration: .1, { morphSVG:"Hand 10"});
+    .to("#Hand 10", duration: .1, { morphSVG:"Hand 11"});
+
+    return tl; 
 }
 
-mainTL.add (AllElements()); 
+mainTL.add (AllElements());
