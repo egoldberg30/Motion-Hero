abc316e1ca513a5ad0bb5c8636606545ffdf40af
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-01 10:32:41 -0400

    updated svg IDs

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -14,15 +14,15 @@ function AllElements(){
 
     tl.from ("#X-rayBoard",{duration: 2, x:"-=1000", ease: "Power3"});
 
-    tl.to("#Hand1", { morphSVG:"Hand 2", duration: .1})
-    .to("#Hand2", { morphSVG:"Hand 2", duration: .1})
-    .to("#Hand3", { morphSVG:"Hand 3", duration: .1})
-    .to("#Hand4", { morphSVG:"Hand 4", duration: .1})
-    .to("#Hand5", { morphSVG:"Hand 5", duration: .1})
-    .to("#Hand6", { morphSVG:"Hand 6", duration: .1})
-    .to("#Hand7", { morphSVG:"Hand 7", duration: .1})
-    .to("#Hand8", { morphSVG:"Hand 8", duration: .1})
-    .to("#Hand8", { morphSVG:"Hand 9", duration: .1})
+    tl.to("#Hand1", { morphSVG:"Hand2", duration: .1})
+    .to("#Hand2", { morphSVG:"Hand2", duration: .1})
+    .to("#Hand3", { morphSVG:"Hand3", duration: .1})
+    .to("#Hand4", { morphSVG:"Hand4", duration: .1})
+    .to("#Hand5", { morphSVG:"Hand5", duration: .1})
+    .to("#Hand6", { morphSVG:"Hand6", duration: .1})
+    .to("#Hand7", { morphSVG:"Hand7", duration: .1})
+    .to("#Hand8", { morphSVG:"Hand8", duration: .1})
+    .to("#Hand8", { morphSVG:"Hand9", duration: .1})
 
     .from ("#Button",{duration: .15, y:"-=2", ease: "circ.in"})
 
