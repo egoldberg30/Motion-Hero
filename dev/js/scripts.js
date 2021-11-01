42f6f7ed915dcc14c0e4db1ccf78bda2f141b8ea
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-01 11:24:07 -0400

    refined button movement

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,16 +15,16 @@ function AllElements(){
     tl.from ("#XrayBoard",{duration: 2, x:"-=1000", ease: "Power3"});
 
     tl.to("#Hand1", { morphSVG:"#Hand2", duration: 1})
-    .to("#Hand2", { morphSVG:"#Hand2", duration: 1})
-    .to("#Hand3", { morphSVG:"#Hand3", duration: 1})
-    .to("#Hand4", { morphSVG:"#Hand4", duration: 1})
-    .to("#Hand5", { morphSVG:"#Hand5", duration: 1})
-    .to("#Hand6", { morphSVG:"#Hand6", duration: 1})
-    .to("#Hand7", { morphSVG:"#Hand7", duration: 1})
-    .to("#Hand8", { morphSVG:"#Hand8", duration: 1})
-    .to("#Hand8", { morphSVG:"#Hand9", duration: 1})
+    // .to("#Hand2", { morphSVG:"#Hand2", duration: 1})
+    // .to("#Hand3", { morphSVG:"#Hand3", duration: 1})
+    // .to("#Hand4", { morphSVG:"#Hand4", duration: 1})
+    // .to("#Hand5", { morphSVG:"#Hand5", duration: 1})
+    // .to("#Hand6", { morphSVG:"#Hand6", duration: 1})
+    // .to("#Hand7", { morphSVG:"#Hand7", duration: 1})
+    // .to("#Hand8", { morphSVG:"#Hand8", duration: 1})
+    // .to("#Hand8", { morphSVG:"#Hand9", duration: 1})
 
-    .from ("#Button",{duration: 1, y:"-=2", ease: "circ.in"})
+    .to ("#Button",{duration: 1.1, y:"1", ease: "back"})
 
     .to("#Hand9", { morphSVG:"#Hand 10", duration: .1})
     .to("#Hand10", { morphSVG:"#Hand 11", duration: .1});
