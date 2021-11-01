2ac7cafac07b1f5a67384445bff194fcd78394d3
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-01 05:42:29 -0400

    Init gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,18 @@
 import { gsap } from "gsap";
 const mainTL = gsap.timeline();
 
-// mainTL.from("#hero",{duration:2, alpha:0});
+// THIS WAS FROM THE PREVIOUS JS ASSIGNMENT FOR THE HERO //
+// mainTL.from("#hero h2",{duration:1, alpha:0});
+// mainTL.from("header",{duration:1, alpha:0, x:"-1000"});
 
-mainTL.from("#hero h2",{duration:1, alpha:0});
 
-mainTL.from("header",{duration:1, alpha:0, x:"-1000"});
+
+function AllElements(){
+    const tl = gsap.timeline();
+
+    tl.from("#All Elements", {duration: 2, x: -1000, ease: "Power3"});
+
+    return tl;
+}
+
+mainTL.add (AllElements()); 
