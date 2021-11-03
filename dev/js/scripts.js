2b37bb1266b49e2fea321e60a8bb8987ced8dd53
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-03 10:27:32 -0400

    tweaks

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,22 +12,25 @@ const mainTL = gsap.timeline();
 function AllElements(){
     const tl = gsap.timeline();
 
-    tl.from ("#XrayBoard",{duration: 2, x:"-=1000"});
+    tl.from ("#Frame8_MainFrame",{duration: 2, x:"-=1000"});
 
-    tl.to("#Hand1", { morphSVG:"#Hand2", duration: 1})
-    // .to("#Hand2", { morphSVG:"#Hand2", duration: 1})
-    // .to("#Hand3", { morphSVG:"#Hand3", duration: 1})
-    // .to("#Hand4", { morphSVG:"#Hand4", duration: 1})
-    // .to("#Hand5", { morphSVG:"#Hand5", duration: 1})
-    // .to("#Hand6", { morphSVG:"#Hand6", duration: 1})
-    // .to("#Hand7", { morphSVG:"#Hand7", duration: 1})
-    // .to("#Hand8", { morphSVG:"#Hand8", duration: 1})
-    // .to("#Hand8", { morphSVG:"#Hand9", duration: 1})
 
-    .to ("#Button",{duration: 1.1, y:"1", ease: "back"})
+    gsap.set(["#Hand1", "#Hand2", "#Hand3", "#Hand4", "#Hand5", "#Hand6", "#Hand7", "#Hand8", "#Hand9", "#Hand10", "#Hand11"], {morphSVG: "#Hand11", ease: "steps (11)"});
 
-    .to("#Hand9", { morphSVG:"#Hand10", duration: .1})
-    .to("#Hand10", { morphSVG:"#Hand11", duration: .1});
+    // tl.to("#Hand1", {morphSVG:"#Hand2", duration: 1})
+    // .to("#Hand2", {morphSVG:"#Hand2", duration: 1})
+    // .to("#Hand3", {morphSVG:"#Hand3", duration: 1})
+    // .to("#Hand4", {morphSVG:"#Hand4", duration: 1})
+    // .to("#Hand5", {morphSVG:"#Hand5", duration: 1})
+    // .to("#Hand6", {morphSVG:"#Hand6", duration: 1})
+    // .to("#Hand7", {morphSVG:"#Hand7", duration: 1})
+    // .to("#Hand8", {morphSVG:"#Hand8", duration: 1})
+    // .to("#Hand8", {morphSVG:"#Hand9", duration: 1})
+
+    // .to ("#Button",{duration: 1.1, y:"1", ease: "back"})
+
+    // .to("#Hand9", {morphSVG:"#Hand10", duration: .1})
+    // .to("#Hand10", {morphSVG:"#Hand11", duration: .1});
 
     return tl; 
 }
