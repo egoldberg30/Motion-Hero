404c5de1ec377998e29f25f609f0539ef46aa64a
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-07 12:27:44 -0500

    outside hands redone

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,7 +15,21 @@ function AllElements(){
     tl.from ("#Frame8_MainFrame",{duration: 2, x:"-=1000"});
 
 
-    gsap.set(["#Hand1", "#Hand2", "#Hand3", "#Hand4", "#Hand5", "#Hand6", "#Hand7", "#Hand8", "#Hand9", "#Hand10", "#Hand11"], {morphSVG: "#Hand11", ease: "steps (11)"});
+    // gsap.set(["#Hand1", "#Hand2", "#Hand3", "#Hand4", "#Hand5", "#Hand6", "#Hand7", "#Hand8", "#Hand9", "#Hand10", "#Hand11"], {morphSVG: "#Hand11", ease: "steps (11)"});
+
+    tl.to("#Hand1", {morphSVG:"#Hand2", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand2", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand3", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand4", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand5", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand6", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand7", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand8", duration: .4})
+    .to("#Hand1", {morphSVG:"#Hand9", duration: .4})
+
+
+
+
 
     // tl.to("#Hand1", {morphSVG:"#Hand2", duration: 1})
     // .to("#Hand2", {morphSVG:"#Hand2", duration: 1})
