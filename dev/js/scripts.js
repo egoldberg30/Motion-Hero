9e89eeef5302d85d1c7316a80bc2bbb48001da6b
egoldberg30 <81780868+egoldberg30@users.noreply.github.com>
2021-11-07 21:03:55 -0500

    timing etc.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -19,21 +19,23 @@ function AllElements(){
 
     // gsap.set(["#Hand1", "#Hand2", "#Hand3", "#Hand4", "#Hand5", "#Hand6", "#Hand7", "#Hand8", "#Hand9", "#Hand10", "#Hand11"], {morphSVG: "#Hand11", ease: "steps (11)"});
 
-    tl.to("#Hand1", {morphSVG:"#Hand2", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand2", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand3", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand4", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand5", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand6", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand7", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand8", duration: .1})
-    .to("#Hand1", {morphSVG:"#Hand9", duration: .1})
+    tl.to("#Hand1", {morphSVG:"#Hand2", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand2", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand3", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand4", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand5", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand6", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand7", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand8", duration: .05})
+    .to("#Hand1", {morphSVG:"#Hand9", duration: .05})
 
-    .to ("#Button",{duration: 1, y:"2", ease: CustomEase.create("custom", "M0,0 C0.354,0.5 0.352,0.5 0.352,0.5 0.352,0.5 0.804,0.032 1,0 ")})
+    .to ("#Button",{duration: .5, y:"2", ease: CustomEase.create("custom", "M0,0 C0.354,0.5 0.352,0.5 0.352,0.5 0.352,0.5 0.804,0.032 1,0 ")})
 
-    .to("#Hand1", {morphSVG:"#Hand10", duration: .2})
-    .to("#Hand1", {morphSVG:"#Hand11", duration: .2})
-    .to("#Hand1", {y:"+=20px", x:"+=25", duration: .2});
+    .to("#Hand1", {morphSVG:"#Hand10", duration: .1})
+    .to("#Hand1", {morphSVG:"#Hand11", duration: .1})
+    .to("#Hand1", {y:"+=20px", x:"+=25", duration: .1})
+
+    // .to("BlackScreen", {alpha:0, duration: .3});
 
     return tl; 
 }
